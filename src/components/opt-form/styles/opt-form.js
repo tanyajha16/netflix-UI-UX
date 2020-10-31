476ca69c6e09764412ae3cc9d8544d770ab18fb8
sagargarg01 <sagargupta1417@gmail.com>
2020-10-31 23:24:41 +0530

    added opt-form

---
+++ src/components/opt-form/styles/opt-form.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components/macro'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  margin-top: 20px;
+  flex-wrap: wrap;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`
+
+export const Input = styled.input`
+  max-width: 450px;
+  width: 100%;
+  border: 1px solid grey;
+  padding: 10px;
+  border-radius: 2px;
+  height: 60px;
+  box-sizing: border-box;
+  margin-right: 1px;
+
+  @media (max-width: 1000px) {
+    height: 50px;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`
+
+export const Break = styled.div`
+  flex-basis: 100%;
+  height: 0;
+`
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  height: 60px;
+  background: #e50914;
+  color: white;
+  text-transform: uppercase;
+  padding: 0 32px;
+  font-size: 26px;
+  border: 0;
+  cursor: pointer;
+
+  img {
+    margin-left: 10px;
+    filter: brightness(0) invert(1);
+    width: 24px;
+
+    @media (max-width: 1000px) {
+      width: 16px;
+    }
+  }
+
+  &:hover {
+    background: #f40612;
+  }
+
+  @media (max-width: 1000px) {
+    height: 45px;
+    font-size: 16px;
+    margin-top: 20px;
+    font-weight: bold;
+  }
+`
+
+export const Text = styled.p`
+  font-size: 19.2px;
+  color: white;
+  text-align: center;
+
+  @media (max-width: 600px) {
+    font-size: 16px;
+    line-height: 22px;
+  }
+`
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import faqsData from '../fixtures/faq.json'
 import { Accordion } from '../components'
+import { OptForm } from '../components'
 
 export function FaqsContainer() {
   return (
@@ -13,6 +14,14 @@ export function FaqsContainer() {
         </Accordion.Item>
       ))}
       <Accordion.Item />
+
+      <OptForm>
+        <OptForm.Input placeholder='Email Address' />
+        <OptForm.Button>Get Started</OptForm.Button>
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   )
 }
