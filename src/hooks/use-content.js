8fa10abe28e0a2dda3d7c71e5a9e3af848cf96f9
sagargarg01 <sagargupta1417@gmail.com>
2020-11-02 13:21:33 +0530

    fetched data from firebase and apply a mice filter on it

---
+++ src/hooks/use-content.js
@@ -0,0 +1,26 @@
+import { useState, useContext, useEffect } from 'react'
+import { FireBaseContext } from '../context/firebase'
+
+export default function useContent(target) {
+  const [content, setContent] = useState([])
+  const { firebase } = useContext(FireBaseContext)
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection(target)
+      .get()
+      .then((snapshot) => {
+        const allContent = snapshot.docs.map((contentObj) => ({
+          ...contentObj.data(),
+          docId: contentObj.id,
+        }))
+        setContent(allContent)
+      })
+      .catch((error) => {
+        console.log(error.message)
+      })
+  }, [])
+
+  return { [target]: content }
+}
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useContent } from '../hooks'
+import selectionFilter from '../utils/selection-filter'
 
 function Browse() {
+  //we need the series and films
+  const { series } = useContent('series')
+  const { films } = useContent('films')
+
+  //we need slides
+  const slides = selectionFilter({ series, films })
+
+  //pass it to the browse container
+  console.log(slides)
+
   return <div>Hello from the browse</div>
 }
 
